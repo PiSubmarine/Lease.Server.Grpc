3cff8d7fceb4546a2f7a27a4a83262b0a3d98aa9
Dmitriy Monakhov <dimamon6032403@gmail.com>
2026-04-20 19:47:12 +0200

    Missing include added

--- test/PiSubmarine/Lease/Server/Grpc/ServerTest.cpp
+++ test/PiSubmarine/Lease/Server/Grpc/ServerTest.cpp
@@ -1,8 +1,8 @@
 #include <gtest/gtest.h>
-
 #include "PiSubmarine/Error/Api/MakeError.h"
 #include "PiSubmarine/Lease/Api/ILeaseIssuer.h"
 #include "PiSubmarine/Lease/Server/Grpc/Server.h"
+#include "PiSubmarine/Lease/Server/Grpc/ErrorCode.h"
 
 namespace PiSubmarine::Lease::Server::Grpc
 {
